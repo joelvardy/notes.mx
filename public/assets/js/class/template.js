e131be9c43fdfb7810c9a5a2fd66e31a5040394a
Joel Vardy <info@joelvardy.com>
2013-01-21 11:33:14 +0000

    Hooked up the user login / registration with logging

--- public/assets/js/class/template.js
+++ public/assets/js/class/template.js
@@ -52,7 +52,73 @@ Template.prototype = {
 
 			submitForm: function(event){
 				event.preventDefault();
-				console.log('Run the registration / login form');
+
+				// Define the email and password
+				var email = $('form #email').val();
+				var password = $('form #password').val();
+
+				// Check whether the email is available (the email has already been registered)
+				notes.user.available(email, function(response){
+
+					// The email has not been previously registered
+					if (response.status) {
+
+						console.log('The user does not have an account');
+
+						// Create the user an account
+						notes.user.create(email, password, function(response){
+
+							// The user was successfully created
+							if (response.status) {
+
+								console.log('the user was successfully created');
+
+								// Log the new user in
+								notes.user.login(email, password, function(response){
+
+									// The user was successfully logged in
+									if (response.status) {
+
+										console.log('the user has been successfully logged in');
+
+									// There was an error logging them in, maybe the internet has died
+									} else {
+										console.log('there was an error loggin the user in');
+									}
+
+								});
+
+							// There was an error creating the user
+							} else {
+								console.log('there was an error creating the user');
+							}
+
+						});
+
+					// The email is registered
+					} else {
+
+						console.log('This email is already registered');
+
+						// Log the user in
+						notes.user.login(email, password, function(response){
+
+							// The user was successfully logged in
+							if (response.status) {
+
+								console.log('the user has been successfully logged in');
+
+							// There was an error logging them in, maybe the internet has died
+							} else {
+								console.log('there was an error loggin the user in');
+							}
+
+						});
+
+					}
+
+				});
+
 			}
 
 		};
