5f8972f0c87daec321a564428914748b966b3099
Joel Vardy <info@joelvardy.com>
2013-01-24 21:00:56 +0000

    Added relative time to front end utility class

--- public/assets/js/class/utility.js
+++ public/assets/js/class/utility.js
@@ -32,11 +32,42 @@ Utility.prototype = {
 
 	},
 
+	relativeDate: function(date) {
+
+		// Load the date
+		date = new Date(date * 1000);
+
+		var difference = new Date() - date;
+
+		var conversions = [
+			['format', 2592000000],
+			['days', 86400000],
+			['hours', 3600000],
+			['minutes', 60000],
+			['seconds', 1000]
+		];
+
+		// Go through conversions until we hit a match
+		for (var i = 0; i < conversions.length; i++) {
+			var result = Math.floor(difference / conversions[i][1]);
+			if (result >= 2) {
+				if (conversions[i][0] == 'format') {
+					return this.formatDate(date / 1000, 'longMonth ordinalNumber year');
+				}
+				return result+' '+conversions[i][0]+' ago';
+			}
+		}
+
+		// If none of the above matched, then no time could have passed
+		return '1 second ago';
+
+	},
+
 	cutString: function(string, characters) {
 
 		string = string.match(/[^\r\n]+/g)[0];
 
-		if(string.length > characters) {
+		if (string.length > characters) {
 			string = string.substr(0, characters - 1);
 			string = string.substr(0, string.lastIndexOf(' '));
 		}
