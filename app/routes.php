bf443a3d8fa921c2700ca2321ea7c8b0c7f15725
Joel Vardy <info@joelvardy.com>
2013-02-01 13:38:28 +0000

    Only use assets CDN when on production server

--- app/routes.php
+++ app/routes.php
@@ -14,7 +14,7 @@
 // Base
 Route::get('/', function()
 {
-	return View::make('index')->with('assetPath', ($_SERVER['SERVER_NAME'] ? '/assets' : 'https://d3gbpkwknfunu5.cloudfront.net'));
+	return View::make('index')->with('assetPath', ($_SERVER['SERVER_NAME'] != 'notes.mx' ? '/assets' : 'https://d3gbpkwknfunu5.cloudfront.net'));
 });
 
 // Login user
