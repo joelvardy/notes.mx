3247a29df6395e302c9b223d74830a3c8a12b4b5
Joel Vardy <info@joelvardy.com>
2014-06-11 10:38:59 +0100

    Added EOL OEF

--- public/assets/js/base.js
+++ public/assets/js/base.js
@@ -1,34 +1,30 @@
-(function() {
+$(document).ready(function() {
 
-	$(document).ready(function() {
+	// Add classes to the notes scope
+	window.notes = {
+		loading: new Loading(),
+		utility: new Utility(),
+		user: new User(),
+		storage: new Storage(),
+		analytics: new Analytics(),
+		cryptography: new Cryptography(),
+		note: new Note(),
+		template: new Template(),
+		route: new Route()
+	};
 
-		// Add classes to the notes scope
-		window.notes = {
-			loading: new Loading(),
-			utility: new Utility(),
-			user: new User(),
-			storage: new Storage(),
-			analytics: new Analytics(),
-			cryptography: new Cryptography(),
-			note: new Note(),
-			template: new Template(),
-			route: new Route()
-		};
+	// Initialise classes
+	window.notes.loading.init();
+	window.notes.user.init();
 
+	notes.user.read(function(data) {
 		// Initialise classes
-		window.notes.loading.init();
-		window.notes.user.init();
-
-		notes.user.read(function(data) {
-			// Initialise classes
-			window.notes.storage.init();
-			window.notes.analytics.init();
-			window.notes.cryptography.init();
-			window.notes.note.init();
-			window.notes.template.init();
-			window.notes.route.init();
-		});
-
+		window.notes.storage.init();
+		window.notes.analytics.init();
+		window.notes.cryptography.init();
+		window.notes.note.init();
+		window.notes.template.init();
+		window.notes.route.init();
 	});
 
-}());
+});
