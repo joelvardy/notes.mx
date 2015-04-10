941ece074e2eafc1c18fb94795193ad2e72fc525
Joel Vardy <info@joelvardy.com>
2015-04-10 19:14:13 +0100

    Updated user transformer

--- app/Joelvardy/Transformers/UserTransformer.php
+++ app/Joelvardy/Transformers/UserTransformer.php
@@ -5,6 +5,7 @@ namespace Joelvardy\Transformers;
 class UserTransformer extends Transformer {
 
     public function transform($user) {
+        $user = (object) $user;
         return [
             'id' => (int) $user->id,
             'email' => (string) $user->email
