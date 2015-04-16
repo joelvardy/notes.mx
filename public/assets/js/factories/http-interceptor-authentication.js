d145476e8d4a515f49995b2e568ffd1563f85326
Joel Vardy <info@joelvardy.com>
2015-04-16 10:56:34 +0100

    Ensure invalid login attempt is not caught by interceptor

--- public/assets/js/factories/http-interceptor-authentication.js
+++ public/assets/js/factories/http-interceptor-authentication.js
@@ -1,9 +1,9 @@
-notesApp.factory('HttpInterceptorAuthentication', ['$q', '$injector', function ($q, $injector) {
+notesApp.factory('HttpInterceptorAuthentication', ['$q', '$injector', 'Api', function ($q, $injector, Api) {
     return {
 
         responseError: function (response) {
 
-            if (response.status === 401) {
+            if (response.status === 401 && response.config.url !== Api.getPath() + '/users/authenticate') {
                 $injector.get('$state').transitionTo('logout');
             }
 
